b24e44a337ea49bb695e592881f13005e9e38942
Joonas Häll <joonas.hall@gmail.com>
2018-09-26 14:59:51 +0300

    Avatar, Color, Hat

--- Pizzeon_server/Models/Color.cs
+++ Pizzeon_server/Models/Color.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 
 namespace Pizzeon_server.Models {
 	public class Color : PurchaseableItem {
-
+		public Guid Id { get; set;}
+		public Guid Name { get; set;}
+		public Guid Price { get; set;}
 	}
 }
