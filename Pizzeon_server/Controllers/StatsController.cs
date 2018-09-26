0773e138d4aada6b0bc435827da61fa601e9078c
ilppi13 <ilppi13@hotmail.com>
2018-09-26 15:22:42 +0300

    stats stuff

---
+++ Pizzeon_server/Controllers/StatsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizzeon_server.Controllers
+{
+    [Route("api/players/{playerId:Guid}/stats")]
+    [ApiController]
+    public class StatsController
+    {
+        StatsProcessor _processor;
+
+        public StatsController(StatsProcessor processor)
+        {
+            _processor = processor;
+        }
+        
+        [HttpGet("{Id:Guid}")]
+        public Task<Stat[]> GetStats(Guid playerid)
+        {
+            return _processor.GetStats(playerid);
+        }
+
+        [HttpPost]
+        public void AddStats(Guid playerid, SessionStats stats)
+        {
+            
+        }
+    }
+}
